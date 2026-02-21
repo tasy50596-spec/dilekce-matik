7b7c59979b2a8233957e314f582fb53491584c98
tasy50596-spec <tasy50596@gmail.com>
2026-02-21 17:36:37 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def generate_dilekce():
         prompt_text = data.get('prompt')
         
         # Yapay zeka modelini çağırıyoruz
-        model = genai.GenerativeModel('gemini-pro')
+        model = genai.GenerativeModel('gemini-1.5-flash')
         response = model.generate_content(prompt_text)
         
         return jsonify({"dilekce": response.text})
